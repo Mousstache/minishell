728dfb63d08232559af9b65da4f2ed28b099207e
Moussa TROIANO <motroian@paul-f4Br4s6.clusters.42paris.fr>
2023-07-03 20:15:04 +0200

    prend moi un yop

--- exec/addspace.c
+++ exec/addspace.c
@@ -6,11 +6,34 @@
 /*   By: motroian <motroian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 17:46:47 by motroian          #+#    #+#             */
-/*   Updated: 2023/06/30 21:02:14 by motroian         ###   ########.fr       */
+/*   Updated: 2023/07/03 20:14:37 by motroian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "minishell.h"
+
+// char	*ft_clear(char *str)
+// {
+// 	int		i;
+// 	int		j;
+// 	char	*fin;
+
+// 	i = -1;
+// 	j = 0;
+// 	fin = malloc(sizeof(char) * ft_strlen(str));
+// 	while(str[++i])
+// 	{
+// 		if ((str[i] == ' ') && (str[i + 1] == ' '))
+// 		{
+// 			i++;
+// 			while (str[i] == ' ')
+// 				i++;
+// 		}
+// 		fin[j++] = str[i];
+// 	}
+// 	fin[j] = 0;
+// 	return (fin);
+// }
 
 int	ft_plus(char *str)
 {
@@ -21,13 +44,13 @@ int	ft_plus(char *str)
 	len = 0;
 	while (str[++i])
 	{
-		if ((str[i] == '|') || (str[i] == '<') || (str[i] == '>'))
-			len+=2;
-		else if ((str[i] == '<' && str[i + 1] == '<') || (str[i] == '>' && str[i + 1] == '>'))
+		if ((str[i] == '<' && str[i + 1] == '<') || (str[i] == '>' && str[i + 1] == '>'))
 		{
-			len++;
+			len+=4;
 			i++;
 		}
+		else if ((str[i] == '|') || (str[i] == '<') || (str[i] == '>'))
+			len+=3;
 		else
 			len++;
 	}
@@ -46,38 +69,26 @@ char *addspace(char *str)
 	final = malloc(sizeof(char) * (ft_plus(str) + 1));
 	while(str[++i])
 	{
-		if ((str[i] == '|') || (str[i] == '<') || (str[i] == '<' && str[i + 1] == '<')
-			|| (str[i] == '>') || (str[i] == '>' && str[i + 1] == '>'))
+		if ((str[i] == '<' && str[i + 1] == '<') || (str[i] == '>' && str[i + 1] == '>'))
 		{
-			// if (str[i - 1] != ' ')
-			// {
-				// final[j] = ' ';
-				// j++;
-			// }
-			if (str[i + 1] != ' ')
-			{
-				final[j] = ' ';
-				j++;
-			}
+			final[j++] = ' ';
+			final[j++] = str[i++];
+			final[j++] = str[i];
+			final[j++] = ' ';
 		}
-		else if ((str[i] == '<' && str[i + 1] == '<') ||  (str[i] == '>' && str[i + 1] == '>'))
+		else if ((str[i] == '|') || (str[i] == '<') || (str[i] == '>'))
 		{
-			// if (str[i - 1] != ' ')
-			// {
-				// final[j] = ' ';
-				// j++;
-			// }
-			if (str[i + 2] != ' ')
-			{
-				final[j] = ' ';
-				j++;
-			}
+			final[j++] = ' ';
+			final[j++] = str[i];
+			final[j++] = ' ';
 		}
-		final[j++] = str[i];
+		else
+			final[j++] = str[i];
 	}
 	final[j] = 0;
-	// printf("APRES {%s} %i | %zu\n", final, ft_plus(str), ft_strlen(final));
+	printf("APRES {%s} %i | %zu\n", final, ft_plus(str), ft_strlen(final));
 	printf("%d\n", ft_plus(str));
 	free(str);
+	// final = ft_clear(final);
 	return (final);
 }
